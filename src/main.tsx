306a2604196df2fa646b18f76b521357f2f8dc02
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-11-24 13:13:06 +0000

    sentry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://b825ed15a3960797b1c49e3fb449fa3c@o4507747429974016.ingest.us.sentry.io/4510420153204736",
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
